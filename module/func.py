782a80e768aa5d546d612c9d5dedb3b473e80bfe
joannachin1202 <100466009+joannachin1202@users.noreply.github.com>
2022-03-23 13:27:10 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -519,7 +519,7 @@ def subject(array):
     text = text_1 +"\n"+ text_2 +"\n"+text_3
     
     try:
-    message = [  #串列
+         message = [  #串列
             TextSendMessage(  
             text = text
             ),
@@ -527,7 +527,7 @@ def subject(array):
             text = "小圖作為森林的嚮導，將竭盡所能地向你推薦雙修輔系灌木、跨域學程洞穴和第二專長小溪中，可能符合你胃口的跨域蘿蔔坑！"
             )
           ]
-    line_bot_api.reply_message(event.reply_token,message)
+        line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
